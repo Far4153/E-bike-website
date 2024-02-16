2be072e18a1e4c2c79ec75f357ef7dfe39b1e3d4
farjana <farjanashaik153@gmail.com>
2024-02-16 23:10:27 +0530

    adding cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "./Navbar";
+import "../styles/Cart.css";
+
 
 function Cart(){
+    
     return (
-        <div>
+        <div className="cart-page">
             <Navbar />
+            <h2>Cart</h2>
+            <div className="tables-class">
+                <div className="items-table">
+                </div>
+                <div className="summary-table">
+                </div>
+            </div>
         </div>
     )
 }
